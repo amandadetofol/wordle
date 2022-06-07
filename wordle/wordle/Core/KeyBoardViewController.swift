56578284b0c13fdcd7a9871884cc8a0b1f05ab29
Amanda Detofol <amandadetofol@MacBook-Pro-de-Amanda.local>
2022-06-07 19:59:46 -0300

    [Wordle] creating personalized keyboard layout

--- wordle/wordle/Core/KeyBoardViewController.swift
+++ wordle/wordle/Core/KeyBoardViewController.swift
@@ -9,9 +9,91 @@ import UIKit
 
 class KeyboardViewController: UIViewController {
     
+    private let letters = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+    private var keys: [[Character]] = []
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 2
+        layout.minimumLineSpacing = 2
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(KeyCollectionViewCell.self, forCellWithReuseIdentifier: KeyCollectionViewCell.identifier)
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.createKeys()
+        self.setupView()
+        self.setupConstraints()
     }
     
+    private func setupView() {
+        self.view.addSubview(collectionView)
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+    }
+    
+    private func createKeys(){
+        for letter in letters {
+            let row = Array(letter)
+            self.keys.append(row)
+        }
+    }
+    
+}
+
+extension KeyboardViewController: UICollectionViewDelegateFlowLayout {
+    
+}
+
+extension KeyboardViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 20
+        let size = (collectionView.frame.size.width-margin)/10
+        return CGSize(width: size, height: size*1.5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let margin: CGFloat = 20
+        let size = (collectionView.frame.size.width-margin)/10
+        let numberOfItems: CGFloat = CGFloat(collectionView.numberOfItems(inSection: section))
+        
+        let inset: CGFloat = (collectionView.frame.size.width - (size * numberOfItems) - (2 * numberOfItems))/2
+  
+        return UIEdgeInsets(top: 2, left: inset, bottom: 2, right: inset)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension KeyboardViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return keys.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return keys[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KeyCollectionViewCell.identifier, for: indexPath) as? KeyCollectionViewCell else { return UICollectionViewCell() }
+        cell.letter = "a"
+        return cell
+    }
 }
---
+++ wordle/wordle/Core/KeyCollectionViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  KeyCollectionViewCell.swift
+//  wordle
+//
+//  Created by Amanda Detofol on 29/05/22.
+//
+
+import UIKit
+
+class KeyCollectionViewCell: UICollectionViewCell {
+    static let identifier: String  = "KeyCollectionViewCell"
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    var letter: String = "" {
+        didSet {
+            self.label.text = self.letter
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame )
+        self.setupView()
+        self.setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setupView() {
+        self.backgroundColor = .darkGray
+        self.layer.cornerRadius = 3
+        self.addSubview(label)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            label.topAnchor.constraint(equalTo: self.topAnchor),
+            label.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+}
